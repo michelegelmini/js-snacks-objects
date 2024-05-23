ba1a76d86ea1453a584e8b6b440f114f6903758c
michelegelmini <michelegelminivr@gmail.com>
2024-05-23 11:37:31 +0200

    add: snack-03-logic

--- snack-03/main.js
+++ snack-03/main.js
@@ -1,1 +1,24 @@
 'use strict'
+
+const animals = [
+    { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+    { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+    { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+    { nome: 'lupo', famiglia: 'canidi', classe: 'mammiferi' },
+    { nome: 'coccodrillo', famiglia: 'crocodylidae', classe: 'reptilia' },
+    { nome: 'delfino', famiglia: 'platanistidae', classe: 'mammiferi' },
+    { nome: 'bisonte', famiglia: 'bovidae', classe: 'mammiferi' },
+]
+
+const mammals = []
+
+for (let i = 0; i < animals.length; i++) {
+    let currentAnimal = animals[i];
+
+    if (currentAnimal.classe === 'mammiferi') {
+        mammals.push(currentAnimal)
+    }
+
+}
+
+console.log(mammals)
